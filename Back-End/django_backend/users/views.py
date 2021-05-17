c04f4b72a7defc20e33f9d22f5c97ccdf6f3a0c2
Rownak <rownak.sust@gmail.com>
2021-05-17 19:51:35 -0400

    backend- registration has some bugs in the verification email

--- Back-End/django_backend/users/views.py
+++ Back-End/django_backend/users/views.py
@@ -35,27 +35,26 @@ class CustomRegisterView(RegisterView):
     def create(self, request, *args, **kwargs):
 
         #Update
-        user = request.data
-        serializer = self.serializer_class(data=user)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(request)
-        user_data = serializer.data
-        user = models.CustomUser.objects.get(email=user_data['email'])
-        token = RefreshToken.for_user(user).access_token
-        current_site = get_current_site(request).domain
-        relativeLink=reverse('email-verify')
+        # user = request.data
+        # serializer = self.serializer_class(data=user)
+        # serializer.is_valid(raise_exception=True)
+        # serializer.save(request)
+        # user_data = serializer.data
+        # user = models.CustomUser.objects.get(email=user_data['email'])
+        # token = RefreshToken.for_user(user).access_token
+        # current_site = get_current_site(request).domain
+        # relativeLink=reverse('email-verify')
+        #
+        # absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        # email_body = 'Hi '+ user.last_name + 'use link below to verify your email\n' + absurl
+        # data = {'email_body': email_body, 'to_email': user.email, 'email_subject': 'Verify your email'}
+        # Util.send_email(data)
 
-        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
-        email_body = 'Hi '+ user.last_name + 'use link below to verify your email\n' + absurl
-        data = {'email_body': email_body, 'to_email': user.email, 'email_subject': 'Verify your email'}
-        Util.send_email(data)
-
-        # orginal_response = super().create(request, *args, **kwargs)
-        # orginal_response = user_data
-        # custom_data = {"message": "Success", "status": "ok"}
-        # orginal_response.data.update(custom_data)
-        # print(orginal_response)
-        return Response(user_data, status=status.HTTP_201_CREATED)
+        orginal_response = super().create(request, *args, **kwargs)
+        custom_data = {"message": "Success", "status": "ok"}
+        orginal_response.data.update(custom_data)
+        print(orginal_response)
+        return orginal_response
 
 class VerifyEmail(generics.GenericAPIView):
     def get(self):
