36be466b823f1f381602fb45af574b953f7458ff
Rownak <rownak.sust@gmail.com>
2021-05-24 21:33:41 +0000

    token expiration limit extend to 2 days

--- Back-End/django_backend/django_backend/settings.py
+++ Back-End/django_backend/django_backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
+import datetime
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -177,8 +179,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    ),
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',),
     'DEFAULT_PERMISSION_CLASSES':(
         'rest_framework.permissions.AllowAny',
 
@@ -189,3 +190,37 @@ REST_USE_JWT = True
 JWT_AUTH_COOKIE = 'my-app-auth' # The cookie key name can be the one you want
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 SITE_ID = 2
+JWT_AUTH = {
+'JWT_ENCODE_HANDLER': 'rest_framework_jwt.utils.jwt_encode_handler',
+
+'JWT_DECODE_HANDLER': 'rest_framework_jwt.utils.jwt_decode_handler',
+
+'JWT_PAYLOAD_HANDLER': 'rest_framework_jwt.utils.jwt_payload_handler',
+
+'JWT_PAYLOAD_GET_USER_ID_HANDLER': 'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+'JWT_RESPONSE_PAYLOAD_HANDLER': 'rest_framework_jwt.utils.jwt_response_payload_handler',
+
+'JWT_SECRET_KEY': SECRET_KEY,
+'JWT_GET_USER_SECRET_KEY': None,
+'JWT_PUBLIC_KEY': None,
+'JWT_PRIVATE_KEY': None,
+'JWT_ALGORITHM': 'HS256',
+'JWT_VERIFY': True,
+'JWT_VERIFY_EXPIRATION': True,
+'JWT_LEEWAY': 0,
+'JWT_EXPIRATION_DELTA': datetime.timedelta(days=10),
+'JWT_AUDIENCE': None,
+'JWT_ISSUER': None,
+
+'JWT_ALLOW_REFRESH': False,
+'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=30),
+
+'JWT_AUTH_HEADER_PREFIX': 'JWT',
+'JWT_AUTH_COOKIE': None,
+
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=15),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=15),
+}
